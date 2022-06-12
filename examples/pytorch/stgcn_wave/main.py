bb38c703fa2ff0fbe82ac57620eda7806ae6a6f7
gabrieleborg26 <gabrieleborg26@gmail.com>
2022-06-12 17:12:48 +0200

    Maltra 6 sensors

--- examples/pytorch/stgcn_wave/main.py
+++ examples/pytorch/stgcn_wave/main.py
@@ -13,7 +13,7 @@ import argparse
 import scipy.sparse as sp
 import wandb
 
-wandb.init(project="stgcn_qtraffic", entity="gabriele26")
+wandb.init(project="stgcn_maltra", entity="gabriele26")
 
 parser = argparse.ArgumentParser(description='STGCN_WAVE')
 parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
@@ -22,9 +22,9 @@ parser.add_argument('--batch_size', type=int, default=50, help='batch size for t
 parser.add_argument('--epochs', type=int, default=50, help='epochs for training  (default: 50)')
 parser.add_argument('--num_layers', type=int, default=9, help='number of layers')
 parser.add_argument('--window', type=int, default=144, help='window length')
-parser.add_argument('--sensorsfilepath', type=str, default='./data/sensor_graph/graph_sensor_qtraffic.txt', help='sensors file path')
-parser.add_argument('--disfilepath', type=str, default='./data/sensor_graph/distance_qtraffic.csv', help='distance file path')
-parser.add_argument('--tsfilepath', type=str, default='./data/qtraffic.h5', help='ts file path')
+parser.add_argument('--sensorsfilepath', type=str, default='./data/sensor_graph/maltra_data/graph_sensor_ids.txt', help='sensors file path')
+parser.add_argument('--disfilepath', type=str, default='./data/sensor_graph/maltra_data/distance_maltra.csv', help='distance file path')
+parser.add_argument('--tsfilepath', type=str, default='./data/maltra_final_sensors.h5', help='ts file path')
 parser.add_argument('--savemodelpath', type=str, default='stgcnwavemodel.pt', help='save model path')
 parser.add_argument('--pred_len', type=int, default=5, help='how many steps away we want to predict')
 parser.add_argument('--control_str', type=str, default='TNTSTNTST', help='model strcture controller, T: Temporal Layer, S: Spatio Layer, N: Norm Layer')
